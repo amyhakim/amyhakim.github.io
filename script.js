086f2635ed87b74faf74a6f879c0e046e3563da0
Amy621 <amyhakim5253@gmail.com>
2025-10-28 20:30:11 -0400

    edits

---
+++ script.js
@@ -0,0 +1,12 @@
+const projectsLink = document.querySelector('.projects-link strong');
+const projectsDropdown = document.querySelector('.projects-dropdown');
+
+document.addEventListener('DOMContentLoaded', function () {
+  const projectsLink = document.querySelector('.projects-link strong');
+  const projectsDropdown = document.querySelector('.projects-dropdown');
+
+  projectsLink.addEventListener('click', function () {
+    const isVisible = projectsDropdown.style.display === 'block';
+    projectsDropdown.style.display = isVisible ? 'none' : 'block';
+  });
+  });
