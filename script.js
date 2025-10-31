6d6cd8815ec9ac517d7ba5e00eb0c371ba3707eb
Amy621 <amyhakim5253@gmail.com>
2025-10-31 16:51:47 -0400

    update

--- script.js
+++ script.js
@@ -8,16 +8,17 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 
   if ('ontouchstart' in window) {
-  const decor = document.querySelector('.decor-top-right');
-  let rotated = false;
+    const decor = document.querySelector('.decor-top-right');
+    let rotated = false;
 
-  decor.addEventListener('click', () => {
-    if (!rotated) {
-      decor.style.transform = 'translate(45%, -40%) rotate(15deg)';
-      rotated = true;
-    } else {
-      decor.style.transform = 'translate(45%, -40%) rotate(0deg)';
-      rotated = false;
-    }
-  });
-}
+    decor.addEventListener('click', () => {
+      if (!rotated) {
+        decor.style.transform = 'translate(45%, -40%) rotate(15deg)';
+        rotated = true;
+      } else {
+        decor.style.transform = 'translate(45%, -40%) rotate(0deg)';
+        rotated = false;
+      }
+    });
+  }
+}); 
