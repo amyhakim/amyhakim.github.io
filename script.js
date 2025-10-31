c586d5173661207a09dec27bbd5f42b973fe5034
Amy621 <amyhakim5253@gmail.com>
2025-10-31 16:54:42 -0400

    adjust js

--- script.js
+++ script.js
@@ -2,16 +2,24 @@ document.addEventListener('DOMContentLoaded', function () {
   const projectsLink = document.querySelector('.projects-link strong');
   const projectsDropdown = document.querySelector('.projects-dropdown');
 
-  projectsLink.addEventListener('click', function () {
-    const isVisible = getComputedStyle(projectsDropdown).display === 'block';
-    projectsDropdown.style.display = isVisible ? 'none' : 'block';
-  });
+  projectsLink.addEventListener('click', function (e) {
+  e.stopPropagation(); 
+  const isVisible = getComputedStyle(projectsDropdown).display === 'block';
 
+  if (isVisible) {
+    projectsDropdown.style.display = 'none';
+  } else {
+    projectsDropdown.style.display = 'block';
+  }
+});
+
+ 
   if ('ontouchstart' in window) {
     const decor = document.querySelector('.decor-top-right');
     let rotated = false;
 
-    decor.addEventListener('click', () => {
+    decor.addEventListener('click', function (e) {
+      e.stopPropagation(); 
       if (!rotated) {
         decor.style.transform = 'translate(45%, -40%) rotate(15deg)';
         rotated = true;
@@ -21,4 +29,4 @@ document.addEventListener('DOMContentLoaded', function () {
       }
     });
   }
-}); 
+});
