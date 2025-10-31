04415d4e1b30e7835ff563464037e49565291694
Amy621 <amyhakim5253@gmail.com>
2025-10-31 16:58:14 -0400

    remove js logic

--- script.js
+++ script.js
@@ -2,31 +2,14 @@ document.addEventListener('DOMContentLoaded', function () {
   const projectsLink = document.querySelector('.projects-link strong');
   const projectsDropdown = document.querySelector('.projects-dropdown');
 
-  projectsLink.addEventListener('click', function (e) {
-  e.stopPropagation(); 
-  const isVisible = getComputedStyle(projectsDropdown).display === 'block';
-
-  if (isVisible) {
-    projectsDropdown.style.display = 'none';
-  } else {
-    projectsDropdown.style.display = 'block';
-  }
-});
-
  
-  if ('ontouchstart' in window) {
-    const decor = document.querySelector('.decor-top-right');
-    let rotated = false;
+  projectsLink.addEventListener('click', function () {
+    const isVisible = getComputedStyle(projectsDropdown).display === 'block';
 
-    decor.addEventListener('click', function (e) {
-      e.stopPropagation(); 
-      if (!rotated) {
-        decor.style.transform = 'translate(45%, -40%) rotate(15deg)';
-        rotated = true;
-      } else {
-        decor.style.transform = 'translate(45%, -40%) rotate(0deg)';
-        rotated = false;
-      }
-    });
-  }
+    if (isVisible) {
+      projectsDropdown.style.display = 'none';
+    } else {
+      projectsDropdown.style.display = 'block';
+    }
+  });
 });
